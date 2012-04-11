738b46df1fa78129eec41290e4e6b435b4031f24
Belmeliani Amina <amouna.b29@gmail.com>
2012-04-12 00:06:48 +0200

    Update My_project/src/Main.java

--- My_project/src/Main.java
+++ My_project/src/Main.java
@@ -1,4 +1,3 @@
-
 public class Main {
 
 	/**
@@ -6,7 +5,7 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		new Affichage();
-
+System.out.println("j'ai fai cette modification a partir du site !!")
 	}
 
 }
